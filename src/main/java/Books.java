c4fd32db4f362fd3d1e025c22a8457fcceff80e7
Iulian Toma <>
2019-06-08 14:10:49 +0200

    classes with content

---
+++ src/main/java/Books.java
@@ -0,0 +1,23 @@
+public class Books extends AbstractContent{
+
+    //Class Books which inherits from AbstractContent.
+
+    //Publisher
+    //ISBN
+
+    private String publisher;
+    private String isbn;
+
+
+    //Method to return the publisher of the book.
+    public String getPublisher(){
+    return publisher;
+}
+
+
+    //Method to return the ISBN.
+    public String getISBN(){
+        return isbn;
+    }
+
+}
---
+++ src/main/java/Movies.java
@@ -0,0 +1,18 @@
+public class Movies extends AbstractContent {
+
+
+    //Class Movies inherits from AbstractContent.
+    private String writers;
+    private String stars;
+
+
+    //Method to return the Writers of the movie.
+    public String getWriters(){
+        return writers;
+    }
+
+    //Method to return the Stars of the movie.
+    public String getStars(){
+        return stars;
+    }
+}
