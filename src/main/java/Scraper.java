54939da1b39982f221e05d083d7854f3e6cfc56f
Unknown <beam_sws@hotmail.com>
2019-06-17 20:22:34 +0200

    Add content to the scraper class

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,5 +1,5 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.DataNode;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -7,6 +7,19 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 
 public class Scraper {
+    //use Crawler to call ListJSONObject.add(Scraper.getContentAsString(getHrefElement()))
+    public JsonObject searchContent(Document doc, String keyword) {
+        JsonObject jsonObject = new JsonObject();
+        Element table = doc.select("table").get(0);
+        Elements rows = table.select("tr");
+        for (int i = 0; i < rows.size(); i++) {
+            if (keyword.equals(rows.select("td").get(i)) || keyword.equals(doc.title())) {
+                jsonObject = new JsonParser().parse(getContentAsString(doc)).getAsJsonObject();
+                break;
+            }
+        }
+        return jsonObject;
+    }
 
     public String getContentAsString(Document doc) {
 
@@ -17,33 +30,40 @@ public class Scraper {
         Elements elements = doc.getElementsByClass("media-details");
         Element table = doc.select("table").get(0); //select the first table.
         Elements rows = table.select("tr");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
         for (int i = 0; i < rows.size(); i++) { //first row is the col names so skip it.
             Element th = rows.select("th").get(i);
+            Element td = rows.select("td").get(i);
             System.out.println(th.text()); // print th value
-            if (th.is("Category")) {
-                Element col = rows.select("td").get(i);
-                //Books books = new Books("").setCategory(col.toString());
-            }
-            if (th.is("Genre")) {
-                Element col = rows.select("td").get(i);
-            }
-            if (th.is("Format")) {
-                Element col = rows.select("td").get(i);
-            }
+            System.out.println(td.text()); // print td value
+            stringBuilder.append(th.text() + ":" + td.text());
 
-            if (th.is("Authors")) {
-                Element col = rows.select("td").get(i);
-            }
-
-            if (th.is("Publisher")) {
-                Element col = rows.select("td").get(i);
-            }
-            if (th.is("ISBN")) {
-                Element col = rows.select("td").get(i);
-            }
         }
-        return "";
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
+//                if (th.is("Category")) {
+//                Element col = rows.select("td").get(i);
+//                //Books books = new Books("").setCategory(col.toString());
+//            }
+//            if (th.is("Genre")) {
+//                Element col = rows.select("td").get(i);
+//            }
+//            if (th.is("Format")) {
+//                Element col = rows.select("td").get(i);
+//            }
+//
+//            if (th.is("Authors")) {
+//                Element col = rows.select("td").get(i);
+//            }
+//
+//            if (th.is("Publisher")) {
+//                Element col = rows.select("td").get(i);
+//            }
+//            if (th.is("ISBN")) {
+//                Element col = rows.select("td").get(i);
+//            }
 }
 
 
